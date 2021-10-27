8fe89e392bb4194faa418f7d7ae89a4b0e2947cd
vandel <vandeliegoc@gmail.com>
2021-10-27 02:11:07 -0500

    change to typescript

--- Frontend/vue-todo/src/main.ts
+++ Frontend/vue-todo/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueAxios from 'vue-axios'
+import axios from 'axios'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
